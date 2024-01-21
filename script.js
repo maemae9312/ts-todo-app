07219df514f67b290f65d44a56d931034e6ddf67
maemae9312 <78950304+maemae9312@users.noreply.github.com>
2024-01-21 17:55:17 +0900

    add-TypeScript

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-document.getElementById('add-task').addEventListener('click',function() {
-    let newTask = document.getElementById('new-task').value;
+"use strict";
+document.getElementById('add-task').addEventListener('click', function () {
+    const newTaskInput = document.getElementById('new-task');
+    let newTask = newTaskInput.value;
     if (newTask.trim() !== '') {
-        let listItem = document.createElement('li');
+        const listItem = document.createElement('li');
         listItem.textContent = newTask;
-
-        let deleteButton = document.createElement('button');
+        const deleteButton = document.createElement('button');
         deleteButton.textContent = '削除';
-        deleteButton.addEventListener('click', function() {
+        deleteButton.addEventListener('click', function () {
             listItem.remove();
         });
-
         listItem.appendChild(deleteButton);
         document.getElementById('tasks').appendChild(listItem);
-
-        document.getElementById('new-task').value = '';
+        newTaskInput.value = '';
     }
 });
